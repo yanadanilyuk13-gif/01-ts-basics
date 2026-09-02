8b75605395b38df2fbfe6b506df3788910d76263
Yana Danilyuk <yanadanilyuk13@gmail.com>
2026-09-02 20:59:41 +0200

    hw-ts

---
+++ src/task-1.ts
@@ -0,0 +1,12 @@
+/*const name = "Alice";
+const age = 30;
+const isOnline = true;
+*/
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
---
+++ src/task-2.ts
@@ -0,0 +1,19 @@
+/*const product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};*/
+
+interface Product {
+  readonly id: number;
+  title: string;
+  description?: string;
+}
+
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
